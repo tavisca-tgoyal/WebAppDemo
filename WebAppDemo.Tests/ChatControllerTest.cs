c8d3582d2f8f7ae3d33399e7fd9acd504f067303
Tushar Goyal <tgoyal@tavisca.in>
2019-08-14 09:28:12 +0530

    Add project files.

---
+++ WebAppDemo.Tests/ChatControllerTest.cs
@@ -0,0 +1,58 @@
+using System;
+using Xunit;
+using FluentAssertions;
+using WebAppDemo.Controllers;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDemo.Tests
+{
+    public class ChatControllerTest
+    {
+        [Fact]
+        public void If_user_enters_Hi_it_should_return_Hello()
+        {
+            var controller = new ChatController();
+            
+            controller.Get("hi").Should().Be("hello");
+
+        }
+
+        [Fact]
+        public void If_user_enters_Hello_it_should_return_Hi()
+        {
+            var controller = new ChatController();
+
+            controller.Get("hello").Should().Be("hi");
+
+        }
+
+        [Fact]
+        public void Print_by_defalut_msg()
+        {
+            var controller = new ChatController();
+            var expected = (ActionResult<IEnumerable<string>>)new string[] { "enter your message" };
+            var actual = controller.Get();
+
+            Assert.Equal(actual, expected);
+            
+
+        }
+
+        [Fact]
+        public void For_any_other_intut_print_the_defalut_message()
+        {
+            var controller = new ChatController();
+            string expected = "I don't know what are you talking about";
+            string actual = controller.Get("gjlkdjl");
+
+            Assert.Equal(expected, actual);
+
+            //controller.Get("abc").Should().Be("I don't know what are you talking about");
+
+        }
+
+
+
+    }
+}
---
+++ WebAppDemo/Controllers/ChatController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppDemo.Controllers
+{
+    [Route("chat")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "enter your message" };
+        }
+
+        [HttpGet("{msg}")]
+        public string Get(string msg)
+        {
+            string output;
+            if (msg == "hi")
+            {
+                output = "hello";
+            }
+            else if (msg == "hello")
+                output = "hi";
+            else
+                return "I don't know what are you talking about";
+
+            return output;
+        }
+    }
+}
